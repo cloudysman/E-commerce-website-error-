6b14f2b40a34a5aeeb97097b3504ff36229e13d5
cloudysman <tronghieu.ghe@gmail.com>
2024-01-29 23:17:57 +0700

    new

--- e_commerce/store/models/product.py
+++ e_commerce/store/models/product.py
@@ -2,11 +2,14 @@ from django.db import models
 from .category import Category
 
 class Product(models.Model):
-    name=models.CharField(max_length=50)
+    name=models.CharField(max_length=50000)
     price=models.IntegerField(default=0)
     category=models.ForeignKey(Category, on_delete=models.CASCADE,default=1)
     description=models.CharField(max_length=50000,default='',blank=True,null=True)
     image=models.ImageField(upload_to='products/')
 
+    @staticmethod
+    def get_all_products():
+        return Product.objects.all()
     def __str__(self):
         return self.name
--- e_commerce/store/views.py
+++ e_commerce/store/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
+from .models.product import Product
 def home(request):
-    return render(request,'home.html')
+    products =Product.get_all_products()
+    #print(products)
+    return render(request,'home.html',{'product':products})
